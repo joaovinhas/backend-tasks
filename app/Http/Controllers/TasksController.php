8c03499380cfdcfb080015c09cb151852af0e737
joaovinhas <joaovitorvinhas22@gmail.com>
2022-11-11 18:39:19 -0400

    Modificacao Add task, criacao das funcoes editar task e deletar task

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -14,29 +14,79 @@ class TasksController extends Controller
 
         $validator = Validator::make($request->all(),[
             'new_task' => 'required|string|max:255',
+            'id_parent' => 'required|integer',
         ]);
 
         if($validator->fails()){
             return response()->json($validator->errors());       
         }else{
 
-            $id = auth()->user()->id;
+            $id_user = auth()->user()->id;
 
-            $task = Tasks::create([
-                'task' => $request['new_task'],
-                'id_parent' => 0,
-                'id_user' => $id,
-                'concluded' => 0,
-                'timer' => null,
-            ]);
+            if($request['id_parent'] == 0){
 
-            return response()->json(['success' => 'Task cadastrada com sucesso!']);
+                $task = Tasks::create([
+                    'task' => $request['new_task'],
+                    'id_parent' => 0,
+                    'id_user' => $id_user,
+                    'concluded' => 0,
+                    'timer' => null,
+                ]);
+
+                return response()->json(['success' => 'Task cadastrada com sucesso!']);
+
+            }else{
+
+                $id_parent = $request['id_parent'];
+
+                $tasks = DB::select("select id from tasks where id_user = '$id_user' and id = '$id_parent'");
+
+                if($tasks){
+
+                    $task = Tasks::create([
+                        'task' => $request['new_task'],
+                        'id_parent' => $id_parent,
+                        'id_user' => $id_user,
+                        'concluded' => 0,
+                        'timer' => null,
+                    ]);
+
+                    return response()->json(['success' => 'Task cadastrada com sucesso!']);
+                }else{
+                    return response()->json(['error' => 'Task alvo não encontrada!']);
+                }
+            }
         }
     }
 
     public function edit_task(Request $request){
 
-        echo("Editando tasks");
+        $validator = Validator::make($request->all(),[
+            'id' => 'required|integer',
+            'task' => 'required|string|max:255',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());       
+        }else{
+
+            $id_user = auth()->user()->id;
+            $id_task = $request['id'];
+
+            $tasks = DB::select("select id from tasks where id_user = '$id_user' and id = '$id_task'");
+
+            if($tasks){
+
+                $validatedData = (['task' => $request['task'] ]);
+
+                Tasks::whereId($id_task)->update($validatedData);
+
+                return response()->json(['success' => 'Task editada!']);
+
+            }else{
+                return response()->json(['error' => 'Task não encontrada!' ]);
+            }
+        }
 
     }
 
@@ -60,7 +110,45 @@ class TasksController extends Controller
 
     public function del_task(Request $request){
 
-        echo("Apagando tasks");
+        $validator = Validator::make($request->all(),[
+            'id' => 'required|integer',
+        ]);
 
+        if($validator->fails()){
+            return response()->json($validator->errors());       
+        }else{
+
+            $id_user = auth()->user()->id;
+            $id_task = $request['id'];
+
+            $tasks = DB::select("select id from tasks where id_user = '$id_user' and id = '$id_task'");
+
+            if($tasks){
+
+                $this->del_tasks($id_task);
+
+                $del_tasks = Tasks::findOrFail($id_task);
+                $del_tasks->delete();
+
+                return response()->json(['success' => 'Task apagada!' ]);
+            }else{
+                return response()->json(['error' => 'Task não encontrada!' ]);
+            }
+        }
     }
+
+    public function del_tasks($id_task){
+
+        $id_user = auth()->user()->id;
+
+        $tasks = DB::select("select id from tasks where id_parent = '$id_task' and id_user = '$id_user' ");
+
+        foreach($tasks as $task){
+            $this->del_tasks($task->id);
+
+            $del_tasks = Tasks::findOrFail($task->id);
+            $del_tasks->delete();
+        }
+    }
+
 }
